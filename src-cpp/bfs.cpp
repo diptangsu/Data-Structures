d979d5274d263dca32ca643b88b252282370045d
dev-niket <47634549+dev-niket@users.noreply.github.com>
2019-10-10 23:24:12 +0530

    Update bfs.cpp
    Added the indentation.

--- src-cpp/bfs.cpp
+++ src-cpp/bfs.cpp
@@ -23,102 +23,101 @@ int isEmpty_queue();
 
 int main()
 {
-create_graph();
-BF_Traversal();
-return 0;
+    create_graph();
+    BF_Traversal();
+    return 0;
 }
  
 void BF_Traversal()
 {
-int v;
-for(v=0; v<n; v++)
-state[v] = initial;
-printf("Enter Start Vertex for BFS: \n");
-scanf("%d", &v);
-BFS(v);
+    int v;
+    for(v=0; v<n; v++)
+    state[v] = initial;
+    printf("Enter Start Vertex for BFS: \n");
+    scanf("%d", &v);
+    BFS(v);
 }
  
 void BFS(int v)
 {
-int i;
-insert_queue(v);
-state[v] = waiting;
-while(!isEmpty_queue())
-{
-v = delete_queue( );
-printf("%c ",(v+65));
-state[v] = visited;
-for(i=0; i<n; i++)
-{
-if(adj[v][i] == 1 && state[i] == initial)
-{
-insert_queue(i);
-state[i] = waiting;
-}
-}
-}
-printf("\n");
+    int i;
+    insert_queue(v);
+    state[v] = waiting;
+    while(!isEmpty_queue())
+    {
+        v = delete_queue( );
+        printf("%c ",(v+65));
+        state[v] = visited;
+        for(i=0; i<n; i++)
+        {
+            if(adj[v][i] == 1 && state[i] == initial)
+            {
+                insert_queue(i);
+                state[i] = waiting;
+            }
+        }
+    }
+    printf("\n");
 }
  
 void insert_queue(int vertex)
 {
-if(rear == MAX-1)
-printf("Queue Overflow\n");
-else
-{
-if(front == -1)
-front = 0;
-rear = rear+1;
-queue[rear] = vertex ;
-}
+    if(rear == MAX-1)
+        printf("Queue Overflow\n");
+    else
+    {
+        if(front == -1)
+        front = 0;
+        rear = rear+1;
+        queue[rear] = vertex ;
+    }
 }
  
 int isEmpty_queue()
 {
-if(front == -1 || front > rear)
-return 1;
-else
-return 0;
+    if(front == -1 || front > rear)
+        return 1;
+    else
+        return 0;
 }
  
 int delete_queue()
 {
-int delete_item;
-if(front == -1 || front > rear)
-{
-printf("Queue Underflow\n");
-exit(1);
-}
-delete_item = queue[front];
-front = front+1;
-return delete_item;
+    int delete_item;
+    if(front == -1 || front > rear)
+    {
+        printf("Queue Underflow\n");
+        exit(1);
+    }
+    delete_item = queue[front];
+    front = front+1;
+    return delete_item;
 }
  
 void create_graph()
 {
-int count,max_edge,origin,destin;
- 
-printf("Enter number of vertices : ");
-scanf("%d",&n);
-max_edge = n*(n-1);
- 
-for(count=1; count<=max_edge; count++)
-{
-printf("Enter edge %d( -1 -1 to quit ) : ",count);
-scanf("%d %d",&origin,&destin);
- 
-if((origin == -1) && (destin == -1))
-break;
- 
-if(origin>=n || destin>=n || origin<0 || destin<0)
-{
-printf("Invalid edge!\n");
-count--;
+    int count,max_edge,origin,destin;
+    
+    printf("Enter number of vertices : ");
+    scanf("%d",&n);
+    max_edge = n*(n-1);
+    
+    for(count=1; count<=max_edge; count++)
+    {
+        printf("Enter edge %d( -1 -1 to quit ) : ",count);
+        scanf("%d %d",&origin,&destin);
+        
+        if((origin == -1) && (destin == -1))
+            break;
+        
+        if(origin>=n || destin>=n || origin<0 || destin<0)
+        {
+            printf("Invalid edge!\n");
+            count--;
+        }
+        else
+        {
+            adj[origin][destin] = 1;
+        }
+    }
 }
-else
-{
-adj[origin][destin] = 1;
-}
-}
-}
-
